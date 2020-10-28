0ee6edb7816a082c747a3e84a461666d53824196
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2020-10-27 21:52:33 -0700

    style: modify margins

--- js/InputForm.js
+++ js/InputForm.js
@@ -2,9 +2,9 @@ import { parser } from './util.js';
 
 const InputForm = (props) => {
     let domString = `
-        <form class="text-center mt-5 text-light">
+        <form class="text-center my-5 text-light">
             <div class="form-group">
-                <label for="question" class="d-block">${props.label}</label>
+                <label for="question" class="d-block mb-4">${props.label}</label>
                 <input type="text" class="form-control w-75 mb-2 ${props.isSubmitted ? 'd-none' : 'd-inline-block'}" id="question">
                 <button type="submit" class="btn btn-outline-light btn-lg ml-2 mb-2 px-4 d-inline-block}">
                     ${props.isSubmitted ? 'Try again' : 'Ask!'}
